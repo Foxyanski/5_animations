7f268bb0bfc60472eb65a792f89a672f09aaabfd
Foxyanski <foxyanski@gmail.com>
2023-03-31 16:39:28 +0300

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const showText = ref(true)
+</script>
+
+<template>
+  <main>
+    <h1 v-if="showText">Hello Japan</h1>
+    <button @click="showText = !showText">Toggle</button>
+  </main>
+</template>
